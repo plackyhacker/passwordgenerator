68d8f6649a8ababfb774df8d9ff2406739f24a96
plackyhacker <42491100+plackyhacker@users.noreply.github.com>
2021-07-05 13:47:42 +0100

    Create generator.py

---
+++ generator.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+from random import seed
+from random import randint
+
+count = 160
+min_word_length = 5
+file = "/usr/share/wordlists/wfuzz/general/common.txt"
+l = []
+
+f = open(file, "r")
+
+for line in f:
+	if len(line.replace("\n", "")) >= min_word_length:
+		l.append(line.replace("\n", ""))
+
+f.close
+
+length_of_array = len(l)
+
+for y in range(count):
+	password = ""
+
+	for x in range(3):
+		rnd = randint(0, length_of_array - 1)
+		password = password + str(l[rnd]) + "-"
+
+	password = password + str(rnd)
+	
+print(password)
